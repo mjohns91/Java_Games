bfedda9dae833d34dc9793d06045816051bfcbb6
Matthew Johnson <mjohns27@ncsu.edu>
2022-12-19 23:41:41 -0500

    edited snake code and added tutorial credits

---
+++ Snake/src/SnakeGame.java
@@ -0,0 +1,8 @@
+//code tutorial from Bro Code https://youtu.be/bI6e6qjJ8JQ
+package Java_Games.Snake.src;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
--- TicTacToe/src/Main.java
+++ TicTacToe/src/Main.java
@@ -1,3 +1,5 @@
+//code tutorial from Bro Code https://youtu.be/rA7tfvpkw0I
+
 public class Main {
 
     public static void main(String[] args) {
